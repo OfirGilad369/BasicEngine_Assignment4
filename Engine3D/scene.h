50ac0fa7634330ba61aad5aa6e610b9a91ff528d
Ofir Gilad <ofir.gilad9631@gmail.com>
2023-05-12 00:27:12 +0300

    fresh upload

---
+++ Engine3D/scene.h
@@ -0,0 +1,117 @@
+#pragma once
+#include "shader.h"
+#include "shape.h"
+#include "camera.h"
+#include "VertexArray.hpp"
+#include <vector>
+
+
+class Scene : public MovableGLM
+{
+
+public:
+	enum axis{xAxis,yAxis,zAxis};
+	enum transformations{xTranslate, yTranslate, zTranslate, xRotate, yRotate, zRotate, xScale, yScale, zScale, xCameraTranslate, yCameraTranslate, zCameraTranslate};
+	enum modes{POINTS, LINES, LINE_LOOP, LINE_STRIP, TRIANGLES, TRIANGLE_STRIP, TRIANGLE_FAN, QUADS};
+	enum buffers{COLOR, DEPTH, STENCIL, BACK, FRONT, ACCUM, NONE };
+	enum shapes{Axis, Plane, Cube, Octahedron, Tethrahedron, LineCopy, MeshCopy};
+	
+	Scene();
+	Scene(float angle,float relationWH,float near, float far);
+	
+	void AddShapeFromFile(const std::string& fileName,int parent,unsigned int mode);
+	virtual void AddShape(int type,int parent,unsigned int mode);
+	void AddShapeCopy(int indx,int parent,unsigned int mode);
+
+	void AddShader(const std::string& fileName);
+	void AddTexture(const std::string& textureFileName, bool for2D);
+	void AddTexture(int width,int height, unsigned char *data);
+
+	void AddCamera(glm::vec3& pos , float fov, float relationWH, float zNear, float zFar);
+
+	void ZeroShapesTrans();
+
+	virtual void Update(const glm::mat4 &MVP,const glm::mat4 &Normal,const int  shaderIndx) = 0;
+	virtual void WhenTranslate(){};
+	virtual void WhenRotate(){};
+	virtual void Motion(){};
+	virtual void Draw(int shaderIndx,int cameraIndx,int buffer,bool toClear,bool debugMode);
+	virtual ~Scene(void);
+
+	void MoveCamera(int cameraIndx,int type,float amt);
+	void ShapeTransformation(int type,float amt);
+
+	float Picking(int x,int y);
+	void Resize(int width,int hight);
+
+	inline void SetParent(int indx,int newValue) {chainParents[indx]=newValue;}
+	inline float GetNear(int cameraIndx){return cameras[cameraIndx]->GetNear();}
+	inline float GetFar(int cameraIndx){return cameras[cameraIndx]->GetFar();}
+
+	void ReadPixel();
+	
+	inline float GetAngle(int cameraIndx) {return cameras[cameraIndx]->GetAngle();}
+	inline void Activate() {isActive = true; }
+	inline void Deactivate() {isActive = false;}
+	void HideShape(int shpIndx);
+	void UnhideShape(int shpIndx);
+
+	void UpdatePosition(float xpos, float ypos);
+	virtual void MouseProccessing(int button);
+	bool inline IsActive() const { return isActive;} 
+	
+	inline void SetShapeTex(int shpIndx,int texIndx){shapes[shpIndx]->SetTexture(texIndx);} 
+	inline void SetShapeShader(int shpIndx,int shdrIndx){shapes[shpIndx]->SetShader(shdrIndx);} 
+
+	// New Functions
+	void MainParameters(int DISPLAY_WIDTH, int DISPLAY_HEIGHT, float CAMERA_ANGLE, float NEAR, float FAR) {
+		width = DISPLAY_WIDTH;
+		height = DISPLAY_WIDTH;
+		camera_angle = CAMERA_ANGLE;
+		camera_near = NEAR;
+		camera_far = FAR;
+	}
+	virtual std::vector<Shape*>* GetShapes() = 0;
+	virtual void AddBezier1DShape(Shape* bezier_1D_line, int parent) = 0;
+	virtual void MouseScrolling(glm::vec3 delta, int mode) = 0;
+
+	glm::vec2 ZBufferConverter(float x, float y, int mode);
+
+	// Getters
+	inline int GetXrel() { return xrel; }
+	inline int GetYrel() { return yrel; }
+	inline float GetXPicked() { return x_picked; }
+	inline float GetYPicked() { return y_picked; }
+	inline int GetWidth() { return width; }
+	inline int GetHeight() { return height; }
+	
+	// Setters
+	void SetXPicked(float x_picked) { this->x_picked = x_picked; }
+	void SetYPicked(float y_picked) { this->y_picked = y_picked; }
+	void SetPickedShape(int pickedShape) { this->pickedShape = pickedShape; }
+
+private:	
+	
+	std::vector<Camera*> cameras; 
+
+	float depth;
+	int xold, yold,xrel, yrel;
+	int cameraIndx;
+	void Clear(float r, float g, float b, float a);
+
+	// New Variables
+	int width, height;
+	float camera_angle, camera_near, camera_far;
+	float picked_depth, x_picked, y_picked;
+
+protected:
+	std::vector<Shape*> shapes;
+	std::vector<Shader*> shaders;
+	std::vector<int> chainParents;
+	std::vector<Texture*> textures;
+	
+	int pickedShape;
+	
+	bool isActive;
+};
+
---
+++ Game/InputManager.h
@@ -0,0 +1,170 @@
+#pragma once   //maybe should be static class
+#include "display.h"
+#include "game.h"
+
+
+	void mouse_callback(GLFWwindow* window,int button, int action, int mods)
+	{	
+		if(action == GLFW_PRESS )
+		{
+			Game *scn = (Game*)glfwGetWindowUserPointer(window);
+			double x2,y2;
+			glfwGetCursorPos(window,&x2,&y2);
+
+			// If S mode is on - Enable Picking
+			if (scn->route_3D_bezier_1D.S_mode) {
+				scn->Picking((int)x2, (int)y2);
+			}
+		}
+	}
+	
+	void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+	{
+		Game *scn = (Game*)glfwGetWindowUserPointer(window);
+		//scn->MyTranslate(glm::vec3(0,0,xoffset),0);
+
+		scn->MouseScrolling(glm::vec3(0, 0, yoffset), 0);
+	}
+	
+	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+		Game *scn = (Game*)glfwGetWindowUserPointer(window);
+
+		if(action == GLFW_PRESS || action == GLFW_REPEAT)
+		{
+			switch (key)
+			{
+				case GLFW_KEY_ESCAPE:			
+					glfwSetWindowShouldClose(window,GLFW_TRUE);
+				break;
+				//case GLFW_KEY_SPACE:
+				//	if(scn->IsActive())
+				//		scn->Deactivate();
+				//	else
+				//		scn->Activate();
+				//break;
+				//
+				//case GLFW_KEY_UP:
+				//	scn->MoveCamera(0,scn->zTranslate,0.4f);
+				//	break;
+				//case GLFW_KEY_DOWN:
+				//	//scn->shapeTransformation(scn->xGlobalRotate,-5.f);
+				//	//cout<< "down: "<<endl;
+				//	scn->MoveCamera(0,scn->zTranslate,-0.4f);
+				//	break;
+				
+				// New Callbacks
+				case GLFW_KEY_2: // Display 2 segments cubic Bezier curve, each segment is a cubic Bezier.
+					scn->route_3D_bezier_1D.NumberOfSegmentsToDisplay(2);
+					break;
+				case GLFW_KEY_3: // Display 3 segments cubic Bezier curve, each segment is a cubic Bezier.
+					scn->route_3D_bezier_1D.NumberOfSegmentsToDisplay(3);
+					break;
+				case GLFW_KEY_4: // Display 4 segments cubic Bezier curve, each segment is a cubic Bezier.
+					scn->route_3D_bezier_1D.NumberOfSegmentsToDisplay(4);
+					break;
+				case GLFW_KEY_5: // Display 5 segments cubic Bezier curve, each segment is a cubic Bezier.
+					scn->route_3D_bezier_1D.NumberOfSegmentsToDisplay(5);
+					break;
+				case GLFW_KEY_6: // Display 6 segments cubic Bezier curve, each segment is a cubic Bezier.
+					scn->route_3D_bezier_1D.NumberOfSegmentsToDisplay(6);
+					break;
+				case GLFW_KEY_SPACE: // ‘Space’ will start animation where the cube moves along the Bezier curve forwardand backward.
+					if (scn->IsActive()) {
+						scn->Deactivate();
+					}
+					else {
+						scn->Activate();
+					}
+					break;
+				case GLFW_KEY_LEFT: // Move to Previous left.
+					if (!scn->route_3D_bezier_1D.S_mode) {
+						scn->SetPickedShape(scn->route_3D_bezier_1D.PreviousShape());
+					}
+					break;
+				case GLFW_KEY_RIGHT: // Rotate the Scene right.
+					if (!scn->route_3D_bezier_1D.S_mode) {
+						scn->SetPickedShape(scn->route_3D_bezier_1D.NextShape());
+					}
+					break;
+				case GLFW_KEY_R: // 'R' moves the camera to the right.
+					scn->MoveCamera(0, scn->xTranslate, 0.4f);
+					break;
+				case GLFW_KEY_L: // 'L' moves the camera to the left.
+					scn->MoveCamera(0, scn->xTranslate, -0.4f);
+					break;
+				case GLFW_KEY_U: // 'U' moves the camera upward.
+					scn->MoveCamera(0, scn->yTranslate, 0.4f);
+					break;
+				case GLFW_KEY_D: // 'D' moves the camera downward
+					scn->MoveCamera(0, scn->yTranslate, -0.4f);
+					break;
+				case GLFW_KEY_B: // 'B' moves the camera backward.
+					scn->MoveCamera(0, scn->zTranslate, 0.4f);
+					break;
+				case GLFW_KEY_F: // 'F' moves the camera forward.
+					scn->MoveCamera(0, scn->zTranslate, -0.4f);
+					break;
+				case GLFW_KEY_C: // Continuity state: begins and ends when the user presses ‘C’. In this state the angle created by p_2, p_3 and p_1 the next segment must be conserved.
+					if (!scn->route_3D_bezier_1D.C_state) {
+						cout << "Continuity state: On" << endl;
+						scn->route_3D_bezier_1D.C_state = true;
+					}
+					else {
+						cout << "Continuity state: Off" << endl;
+						scn->route_3D_bezier_1D.C_state = false;
+					}
+					break;
+				case GLFW_KEY_S: // Unpicked mode, when 'S' is pressed.In this mode the scene will move by moving the mouse(right button for translationand left button for rotation).
+					if (!scn->route_3D_bezier_1D.S_mode) {
+						cout << "Unpicked mode: On" << endl;
+						cout << "Notice: Scene movement is enabled!" << endl;
+						scn->route_3D_bezier_1D.S_mode = true;
+					}
+					else {
+						scn->SetPickedShape(scn->route_3D_bezier_1D.picked_shape_index);
+						cout << "Unpicked mode: Off" << endl;
+						cout << "Warning: Scene movement is disabled!" << endl;
+						scn->route_3D_bezier_1D.S_mode = false;
+					}
+					break;
+			default:
+				break;
+			}
+		}
+	}
+	
+	void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+	{
+		Game *scn = (Game*)glfwGetWindowUserPointer(window);
+
+		scn->UpdatePosition((float)xpos,(float)ypos);
+
+		// Translate a control point inside the camera plane
+		// When the first control point is translated the cube will follow it (in case the cube is covering the control point)
+		if(glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+		{
+			scn->MouseProccessing(GLFW_MOUSE_BUTTON_RIGHT);
+		}
+		// Rotate a control point that doesn’t locate on the curve (p_1, p_2) around the adjacent control point on the curve (p_0, p_3).
+		// If the user presses on control points p_0 with the left mouse button it will cause p_2 of the previous Bezier path, to be on the same line of p_0 and p_1.
+		else if(glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+		{
+			scn->MouseProccessing(GLFW_MOUSE_BUTTON_LEFT);
+		}
+	}
+
+	void window_size_callback(GLFWwindow* window, int width, int height)
+	{
+		Game *scn = (Game*)glfwGetWindowUserPointer(window);
+
+		scn->Resize(width,height);
+		
+	}
+
+	void Init(Display &display)
+	{
+		display.AddKeyCallBack(key_callback);
+		display.AddMouseCallBacks(mouse_callback,scroll_callback,cursor_position_callback);
+		display.AddResizeCallBack(window_size_callback);
+	}
